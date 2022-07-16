c5c6750bdf9dd2aec032382b5a299bfee54973cd
Pixelatt <chloeting77@gmail.com>
2022-07-16 10:56:23 +0800

    Initial Commit

---
+++ Dog App/Dog_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_AppApp.swift
+//  Dog App
+//
+//  Created by T Krobot on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Dog_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
